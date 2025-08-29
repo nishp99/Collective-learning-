a10c3db38fc20af80d6ba2ad58af469ca7f472cd
nishp99 <nishil.patel.21@ucl.ac.uk>
2025-08-29 12:36:10 +0000

    ode

--- main_ode.py
+++ main_ode.py
@@ -50,35 +50,20 @@ def order_params_ode(student_1, student_2, teacher, D):
 """
 functions to calculate expectations of relevant aligning field quantities
 """
-def ModNu(S):
-    return jnp.sqrt(2 * S/ jnp.pi)
-
-def ModLambda(Q):
+def ModX(Q):
     return jnp.sqrt(2 * Q / jnp.pi)
 
-def SgnLambda_Nu(J, S):
-    return jnp.sqrt(2 / jnp.pi) * J / jnp.sqrt(S)
+def SgnX_Y(Q_x, J_xy):
+    return jnp.sqrt(2 / jnp.pi) * J_xy / jnp.sqrt(Q_x)
 
-def SgnLambdai_Lambdaj(Q_i, Q_ij):
-    return jnp.sqrt(2 / jnp.pi) * Q_ij / jnp.sqrt(Q_i)
+def SgnXY(Q_x, Q_y, J_xy):
+    return 2 / jnp.pi * jnp.arcsin(J_xy / jnp.sqrt(Q_x * Q_y))
 
-def SgnNu_Lambda(J, Q):
-    return jnp.sqrt(2 / jnp.pi) * J / jnp.sqrt(Q)
-
-def Sgn12(Q_1, Q_2, Q_12):
-    return 2 / jnp.pi * jnp.arcsin(Q_12 / jnp.sqrt(Q_1 * Q_2))
-
-def SgnNuLambda(J, Q, S):
-    return 2 / jnp.pi * jnp.arcsin(J / jnp.sqrt(Q * S))
-
-def Rho_12(J_1, J_2, Q_1, Q_2, Q_12, S):
-    return (Q_12 * S - J_1 * J_2) / jnp.sqrt((Q_1 * S - J_1**2) * (Q_2 * S - J_2**2))
-
-def Rho_lambda(J_i, J_j, Q_i, Q_j, Q_12, S):
-    return (J_i * Q_j - J_j * Q_12) / jnp.sqrt((Q_i * S - J_i**2) * (Q_j * S - J_j**2))
+def RhoXY_Z(Q_x, Q_y, Q_z, J_xz, J_yz, J_xy):
+    return (J_xy * Q_z - J_xz * J_yz) / jnp.sqrt((Q_x * Q_z - J_xz**2) * (Q_y * Q_z - J_yz**2))
 
 def HeaviLambdaNu(J, Q):
-    return 1 - 1 / jnp.pi * jnp.arccos(J / Q)
+    return 1 - 1 / jnp.pi * jnp.arccos(J / jnp.sqrt(Q))
     
 def Heavi12_heavi12Nu(J_1, J_2, Q_1, Q_2, Q_12, S):
     prob = 1/4 + 1/(2 * jnp.pi) * (jnp.arcsin(Q_12 / jnp.sqrt(Q_1*Q_2)) + jnp.arcsin(J_1/jnp.sqrt(Q_1*S)) + jnp.arcsin(J_2/jnp.sqrt(Q_2*S)))
@@ -88,27 +73,51 @@ def Heavi12_negheavi12Nu(J_1, J_2, Q_1, Q_2, Q_12, S):
     prob = 1/(2 * jnp.pi) * (-jnp.arccos(Q_12 / jnp.sqrt(Q_1*Q_2)) + jnp.arccos(J_1/jnp.sqrt(Q_1*S)) + jnp.arccos(J_2/jnp.sqrt(Q_2*S)))
     return prob
 
-def Sgn12_modNu(J_1, J_2, Q_1, Q_2, Q_12, S):
-    return (jnp.sqrt(2 / jnp.pi) * (J_1/jnp.sqrt(Q_1) + J_2/jnp.sqrt(Q_2) + jnp.sqrt(S)) - 
-            4/(jnp.pi * jnp.sqrt(2*jnp.pi)) * (J_1/jnp.sqrt(Q_1) * jnp.arccos(Rho_lambda(J_1, J_2, Q_1, Q_2, Q_12, S)) +
-                        J_2/jnp.sqrt(Q_2) * jnp.arccos(Rho_lambda(J_2, J_1, Q_2, Q_1, Q_12, S)) +
-                        jnp.sqrt(S) * jnp.arccos(Rho_12(J_1, J_2, Q_1, Q_2, Q_12, S))))
+def SgnXY_modZ(Q_x, Q_y, Q_z, J_xz, J_yz, J_xy):
+    return 2 * jnp.sqrt(2) / (jnp.pi * jnp.sqrt(jnp.pi)) * (jnp.sqrt(Q_z) * jnp.arcsin(RhoXY_Z(Q_z, Q_y, Q_x, J_xz, J_xy, J_yz)) +
+                        J_xz / jnp.sqrt(Q_x) * jnp.arcsin(RhoXY_Z(Q_z, Q_x, Q_y, J_yz, J_xy, J_xz)) +
+                        J_yz / jnp.sqrt(Q_y) * jnp.arcsin(RhoXY_Z(Q_x, Q_y, Q_z, J_xz, J_yz, J_xy)))
 
-def SgniNu_modj(J_i, J_j, Q_i, Q_j, Q_12, S):
-    return 2 * jnp.sqrt(2) / (jnp.pi * jnp.sqrt(jnp.pi)) * (jnp.sqrt(Q_j) * jnp.arcsin(Rho_lambda(J_j, J_i, Q_j, Q_i, Q_12, S)) +
-                        Q_12 / jnp.sqrt(Q_i) * jnp.arcsin(Rho_lambda(J_i, J_j, Q_i, Q_j, Q_12, S)) +
-                        J_j / jnp.sqrt(S) * jnp.arcsin(Rho_12(J_i, J_j, Q_i, Q_j, Q_12, S)))
+def Compute_expectations(Q_1, Q_2, Q_12, J_1, J_2, S):
+    P_1 = HeaviLambdaNu(J_1, Q_1)
+    P_2 = HeaviLambdaNu(J_2, Q_2)
+    P_12 = Heavi12_heavi12Nu(J_1, J_2, Q_1, Q_2, Q_12, S)
+    P_12_collab = Heavi12_negheavi12Nu(J_1, J_2, Q_1, Q_2, Q_12, S)
 
+    sgn12_modNu = SgnXY_modZ(Q_1, Q_2, S, J_1, J_2, Q_12)
+    sgn1Nu_mod2 = SgnXY_modZ(Q_1, S, Q_2, Q_12, J_2, J_1)
+    sgn2Nu_mod1 = SgnXY_modZ(Q_2, S, Q_1, Q_12, J_1, J_2)
+
+    sgn1_Nu = SgnX_Y(Q_1, J_1)
+    sgn2_Nu = SgnX_Y(Q_2, J_2)
+    sgnNu_1 = SgnX_Y(S, J_1)
+    sgnNu_2 = SgnX_Y(S, J_2)
+    sgn1_2 = SgnX_Y(Q_1, Q_12)
+    sgn2_1 = SgnX_Y(Q_2, Q_12)
+
+    sgn12 = SgnXY(Q_1, Q_2, Q_12)
+    sgnNu1 = SgnXY(Q_1, S, J_1)
+    sgnNu2 = SgnXY(Q_2, S, J_2)
+
+    mod1 = ModX(Q_1)
+    mod2 = ModX(Q_2)
+    modNu = ModX(S)
+
+    return P_1, P_2, P_12, P_12_collab, \
+        sgn12_modNu, sgn1Nu_mod2, sgn2Nu_mod1, \
+            sgn1_Nu, sgn2_Nu, sgnNu_1, sgnNu_2, sgn1_2, sgn2_1, \
+                sgn12, sgnNu1, sgnNu2, \
+                    mod1, mod2, modNu
     
 """
-calculate order parameter updates. YO NISH, I changed a sign somewhere, good luck finding it :(
+calculate order parameter updates
 """
 def dJ_i(T, eta, r_i, r_j, r_12, tau_j, sgni_Nu, sgnj_Nu, sgn12_modNu, modNu, P_i, P_j, P_12_collab):
     dj = eta * (r_i / 2 * P_i**(T - 1) * (sgni_Nu + modNu) + tau_j * r_j / 2 * P_j**(T - 1) * (sgn12_modNu + sgni_Nu)
                   + r_12 / 4 * P_12_collab**(T - 1) * (sgni_Nu + sgnj_Nu - sgn12_modNu - modNu))
     return dj
 
-def dQ_i(T, eta, r_i, r_j, r_12, tau_j, modi, modj, sgnNu_i, sgnjNu_modi, sgnj_i, 
+def dQ_i(T, eta, r_i, r_j, r_12, tau_j, modi, sgnNu_i, sgnjNu_modi, sgnj_i, 
          P_i, P_j, P_12, P_12_collab):
     dq = (eta * (r_i * P_i**(T - 1) * (modi + sgnNu_i) + tau_j * r_j * P_j**(T - 1) * (modi + sgnjNu_modi) + 
                  r_12 / 2 * P_12_collab**(T - 1) * (modi + sgnj_i - sgnjNu_modi - sgnNu_i)) + 
@@ -134,107 +143,64 @@ def update_D_times(dt, eta, T, r_1, r_2, r_12, tau_1, tau_2, J_1, J_2, Q_1, Q_2,
     def body(_, state):
         J_1, J_2, Q_1, Q_2, Q_12 = state
 
-        P_1 = HeaviLambdaNu(J_1, Q_1)
-        P_2 = HeaviLambdaNu(J_2, Q_2)
-        P_12 = Heavi12_heavi12Nu(J_1, J_2, Q_1, Q_2, Q_12, S)
-        P_12_collab = Heavi12_negheavi12Nu(J_1, J_2, Q_1, Q_2, Q_12, S)
-
-        sgn12_modNu = Sgn12_modNu(J_1, J_2, Q_1, Q_2, Q_12, S)
-        sgn1Nu_mod2 = SgniNu_modj(J_1, J_2, Q_1, Q_2, Q_12, S)
-        sgn2Nu_mod1 = SgniNu_modj(J_2, J_1, Q_2, Q_1, Q_12, S)
-
-        sgn1_Nu = SgnLambda_Nu(J_1, S)
-        sgn2_Nu = SgnLambda_Nu(J_2, S)
-        sgnNu_1 = SgnNu_Lambda(J_1, Q_1)
-        sgnNu_2 = SgnNu_Lambda(J_2, Q_2)
-        sgn1_2 = SgnLambdai_Lambdaj(Q_1, Q_12)
-        sgn2_1 = SgnLambdai_Lambdaj(Q_2, Q_12)
-
-        sgn12 = Sgn12(Q_1, Q_2, Q_12)
-        sgnNu1 = SgnNuLambda(J_1, Q_1, S)
-        sgnNu2 = SgnNuLambda(J_2, Q_2, S)
-
-        mod1 = ModLambda(Q_1)
-        mod2 = ModLambda(Q_2)
-        modNu = ModNu(S)
+        P_1, P_2, P_12, P_12_collab, \
+        sgn12_modNu, sgn1Nu_mod2, sgn2Nu_mod1, \
+        sgn1_Nu, sgn2_Nu, sgnNu_1, sgnNu_2, sgn1_2, sgn2_1, \
+        sgn12, sgnNu1, sgnNu2, \
+        mod1, mod2, modNu = Compute_expectations(Q_1, Q_2, Q_12, J_1, J_2, S)
                
         dJ_1 = dJ_i(T, eta, r_1, r_2, r_12, tau_2, sgn1_Nu, sgn2_Nu, sgn12_modNu, modNu, P_1, P_2, P_12_collab) * dt
         dJ_2 = dJ_i(T, eta, r_2, r_1, r_12, tau_1, sgn2_Nu, sgn1_Nu, sgn12_modNu, modNu, P_2, P_1, P_12_collab) * dt
-        dQ_1 = dQ_i(T, eta, r_1, r_2, r_12, tau_2, mod1, mod2, sgnNu_1, sgn2Nu_mod1, sgn2_1, P_1, P_2, P_12, P_12_collab) * dt
-        dQ_2 = dQ_i(T, eta, r_2, r_1, r_12, tau_1, mod2, mod1, sgnNu_2, sgn1Nu_mod2, sgn1_2, P_2, P_1, P_12, P_12_collab) * dt
+        dQ_1 = dQ_i(T, eta, r_1, r_2, r_12, tau_2, mod1, sgnNu_1, sgn2Nu_mod1, sgn2_1, P_1, P_2, P_12, P_12_collab) * dt
+        dQ_2 = dQ_i(T, eta, r_2, r_1, r_12, tau_1, mod2, sgnNu_2, sgn1Nu_mod2, sgn1_2, P_2, P_1, P_12, P_12_collab) * dt
         dQ_12 = dQ_ij(T, eta, r_1, r_2, r_12, tau_1, tau_2, mod1, mod2, sgn1_2, sgn2_1, sgnNu_1, sgnNu_2, sgn12, sgnNu1, 
                       sgnNu2, sgn1Nu_mod2, sgn2Nu_mod1, P_1, P_2, P_12, P_12_collab) * dt
 
-        J_1 = J_1 + dJ_1
-        J_2 = J_2 + dJ_2
-        Q_1 = Q_1 + dQ_1
-        Q_2 = Q_2 + dQ_2
-        Q_12 = Q_12 + dQ_12
-
-        return J_1, J_2, Q_1, Q_2, Q_12
+        return J_1 + dJ_1, J_2 + dJ_2, Q_1 + dQ_1, Q_2 + dQ_2, Q_12 + dQ_12
 
     return lax.fori_loop(0, D, body, (J_1, J_2, Q_1, Q_2, Q_12))
 
 """
-jittable way for finding corresponding index
+ODE solver function
 """
-def lookup_index(x, keys, values):
-    match = keys == x + 1
-    exists = jnp.any(match)              # bool[]
-    first_idx = jnp.argmax(match)        # int[]
-    return jnp.where(exists, values[first_idx], -1)
-
-"""
-numerically solve the ODE for given parameters for given number of epochs,
-storing savepoints number of points logarithmically spaced
-"""
-def ODE_solver(eta, T, r_1, r_2, r_12, tau_1, tau_2, dt, epochs, savepoints, 
+def ODE_solver(eta, T, r_1, r_2, r_12, tau_1, tau_2, dt, epochs, savepoints,
                J_1_init=1e-4, J_2_init=1e-4, Q_1_init=1, Q_2_init=1, Q_12_init=1e-4, S=1):
-    
-    J_1 = J_1_init
-    J_2 = J_2_init
-    Q_1 = Q_1_init
-    Q_2 = Q_2_init
-    Q_12 = Q_12_init
-    S = S
+    """
+    Numerically solve the ODE for given parameters, storing at logarithmically spaced savepoints.
+    """
 
-    log_keys = np.unique(np.logspace(0, np.log10(epochs - 1), num=savepoints, dtype=int))
-    save_number = len(log_keys)
-    log_keys = jnp.array(log_keys)
-    log_values = jnp.arange(save_number, dtype=int)
+    # ---- Precompute log-spaced epoch indices ----
+    save_epochs = np.unique(np.logspace(0, np.log10(epochs - 1), num=savepoints, dtype=int))
+    save_epochs = jnp.array(save_epochs)
+    deltas = jnp.diff(save_epochs, prepend=0)  # number of steps between savepoints
+    num_saves = save_epochs.shape[0]
 
-    def body(e, carry):
-        J_1_s, J_2_s, Q_1_s, Q_2_s, Q_12_s, J_1, J_2, Q_1, Q_2, Q_12 = carry
+    # ---- Initial state ----
+    init_state = (J_1_init, J_2_init, Q_1_init, Q_2_init, Q_12_init)
 
-        index = lookup_index(e, log_keys, log_values)
-        J_1_s = J_1_s.at[index].set(J_1)
-        J_2_s = J_2_s.at[index].set(J_2)
-        Q_1_s = Q_1_s.at[index].set(Q_1)
-        Q_2_s = Q_2_s.at[index].set(Q_2)
-        Q_12_s = Q_12_s.at[index].set(Q_12)
+    # ---- One scan step = integrate between savepoints ----
+    def scan_body(carry, delta):
+        J_1, J_2, Q_1, Q_2, Q_12 = carry
 
-        J_1, J_2, Q_1, Q_2, Q_12 = update_D_times(dt, eta, T, r_1, r_2, r_12, tau_1, tau_2, J_1, J_2, Q_1, Q_2, Q_12, S)
+        # integrate "delta" epochs by repeatedly applying update_D_times
+        def inner_loop(_, state):
+            return update_D_times(dt, eta, T, r_1, r_2, r_12, tau_1, tau_2, *state, S)
 
-        return J_1_s, J_2_s, Q_1_s, Q_2_s, Q_12_s, J_1, J_2, Q_1, Q_2, Q_12
+        new_state = lax.fori_loop(0, delta, inner_loop, (J_1, J_2, Q_1, Q_2, Q_12))
 
-    #create logspaced index array
-    #indices = jnp.logspace(0, jnp.log10(time_steps), num=100, dtype=int)
-    J_1_s = jnp.zeros(save_number+1, dtype=jnp.float32)
-    J_2_s = jnp.zeros(save_number+1, dtype=jnp.float32)
-    Q_1_s = jnp.zeros(save_number+1, dtype=jnp.float32)
-    Q_2_s = jnp.zeros(save_number+1, dtype=jnp.float32)
-    Q_12_s = jnp.zeros(save_number+1, dtype=jnp.float32)
+        return new_state, new_state
 
-    J_1_s, J_2_s, Q_1_s, Q_2_s, Q_12_s, _, _, _, _, _ = lax.fori_loop(
-        0, epochs,
-        body,
-        (J_1_s, J_2_s, Q_1_s, Q_2_s, Q_12_s, J_1, J_2, Q_1, Q_2, Q_12)
-    )
+    # ---- Run scan over deltas ----
+    _, trajectory = lax.scan(scan_body, init_state, deltas)
 
-    #return the storing arrays
-    return J_1_s[:-1], J_2_s[:-1], Q_1_s[:-1], Q_2_s[:-1], Q_12_s[:-1]
-ODE_solver = jit(ODE_solver, static_argnums=(7, 8, 9))
+    # ---- Split results ----
+    J_1_s, J_2_s, Q_1_s, Q_2_s, Q_12_s = trajectory
+    return J_1_s, J_2_s, Q_1_s, Q_2_s, Q_12_s
+
+# JIT (same as before)
+ODE_solver = jit(ODE_solver, static_argnums=(7, 8, 9, 15))
 vmapped_ODE_solver = jax.vmap(ODE_solver, in_axes=(0, 0, 0, 0, 0, 0, 0, None, None, None))
+
 
 
 def main_batched(args):
